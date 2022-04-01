200ede001a91d1e24db89d72ae8bd34fbd0348ad
fordez <fordez@kali.fordez>
2022-04-01 02:16:10 -0500

    Data capture and files

---
+++ index.js
@@ -0,0 +1,27 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
+
+client.on('message', message => {
+	console.log(message);
+    client.sendMessage(message.from, 'pong');
+});
+
+client.on('message', async msg => {
+    if(msg.hasMedia) {
+        const media = await msg.downloadMedia();
+        console.log('descarga de medios')
+        console.log(media)
+    }
+});
